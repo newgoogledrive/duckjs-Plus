37e6d638cbf8691ac45c634b92d864d2daeb988d
7Locking <mmttsullivan3@gmail.com>
2025-12-04 08:31:50 -0500

    Delete public directory

--- public/static/app.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('urlInput');
-const button = document.getElementById('goButton');
-const iframe = document.getElementById('proxyFrame');
-
-button.addEventListener('click', () => {
-    let url = input.value.trim();
-    if (!url.startsWith('http')) url = 'https://' + url;
-    iframe.src = '/proxy/' + url;
-});
